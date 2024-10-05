c30ee630aaa26ac75634d926e083b65000149409
Daniel Ahn <dahnhome@d207-023-180-095.wireless.sfu.ca>
2024-10-05 15:16:03 -0700

    sign up and login template, main js file page pathways

--- main.js
+++ main.js
@@ -12,6 +12,18 @@ app.get("/", (req, res) => {
     res.render("home.ejs");
 })
 
+app.get("/globalrank", (req, res) => {
+    res.render("globalrank.ejs");
+});
+
+app.get("/newest", (req, res) => {
+    res.render("newest.ejs");
+});
+
+app.get("/forum", (req, res) => {
+    res.render("forum.ejs");
+});
+
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
 });
